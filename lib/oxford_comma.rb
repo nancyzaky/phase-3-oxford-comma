1a46ef71e44ee21aeefc0837f36414b379f03c80
Nancy Zaky <nancyzki@yahoo.com>
2021-08-31 11:02:44 -0700

    commit tree f309dcbca1da59074535935e1005b05b6d45de8c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+newword = ""
+array.each_with_index do |word, index|
+  if array.length == 1
+    newword << word
+    break
+  end
+  if array.length == 2
+    newword << array[0] + " and "+ array[1]
+    break
+  end
+  if index == array.length - 1
+    newword << "and " + word
 
+  elsif index != array.length - 1 && array.length != 2
+  newword << word + ", "
+  end
+end
+newword
 end
